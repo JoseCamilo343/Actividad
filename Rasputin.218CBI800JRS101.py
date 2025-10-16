0de40b608035053fc1159ba69563f66b4c7476a3
Personal <joserodriguezortegon@gmail.com>
2025-10-16 13:56:50 -0500

    Calculadora

--- Rasputin.218CBI800JRS101.py
+++ Rasputin.218CBI800JRS101.py
@@ -1,38 +1,23 @@
-def sumar(a, b):
-    if b == 0:
-        return a
+digitos = []
+# Función para sumar los dígitos
+def suma_digitos(n):
+    if n < 10:
+        digitos.append(n) 
+        return n
     else:
-        return sumar(a + 1, b - 1)
-def restar(a, b):
-    if b == 0:
-        return a
+        ultimo = n % 10
+        digitos.append(ultimo)
+        resto = n // 10         
+        return ultimo + suma_digitos(resto)
+# Programa principal
+def main():
+    print(" Suma de los digitos de numeros enteros (*---positivos---*)")
+    n = int(input("Ingresa un número entero positivo: "))
+    if n < 0:
+        print("Por favor, ingresa un número positivo.")
     else:
-        return restar(a - 1, b - 1)
-def multiplicar(a, b):
-    if b == 0:
-        return 0
-    else:
-        return a + multiplicar(a, b - 1)
-def dividir(a, b):
-    if b == 0:
-        return "Error: no se puede dividir por cero"
-    if a < b:
-        return 0
-    else:
-        return 1 + dividir(a - b, b)
-operaciones = [
-    [1, "Sumar", sumar], [2, "Restar", restar], [3, "Multiplicar", multiplicar], [4, "Dividir", dividir]]
-def menu():
-    while True:
-        print("\n CALCULADORA ")
-        for option in operaciones:
-            print(f"{option[0]} {option[1]}")
-        print("5. Salir"); Elige = input("Elige una opción : ")
-        if Elige == "5": 
-            print("Gracias por usar la calculadora.")
-            break
-        if Elige in ["1", "2", "3", "4"]: 
-            a = int(input("Número a: ")); b = int(input("Número b: ")); indice = int(Elige) - 1; funcion = operaciones[indice][2]; print("Resultado =", funcion(a, b))
-        else: 
-            print("Opción no valida, porfavor intentalo de nuevo.")
-menu()
+        total = suma_digitos(n)
+        digitos.reverse()  
+        print("\nDígitos encontrados:", digitos)
+        print("La suma de los dígitos es:", total)
+main()
